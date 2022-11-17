a27967f26ef710e0eaa78c67650104b8ecb79189
penguinboots <sabrina.ynw@gmail.com>
2022-11-16 20:30:13 -0800

    Add flatten.js

---
+++ flatten.js
@@ -0,0 +1,33 @@
+// Return true if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Check if two arrays are equal
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const flatten = function(arr) {
+  let flattened = [];
+  for (let obj of arr) {
+    if (Array.isArray(obj)) {
+      flattened = flattened.concat(obj);
+    } else {
+      flattened.push(obj);
+    }
+  }
+  return flattened;
+};
+
+
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6], "a", ["a", 2]]), [1, 2, 3, 4, 5, 6, "a", "a", 2]);
