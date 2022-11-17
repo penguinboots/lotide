13c118ffacb558d94c817353200f01e3e4f3b845
penguinboots <sabrina.ynw@gmail.com>
2022-11-16 20:08:23 -0800

    Add comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,8 +8,11 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+// Check if two arrays are equal
 const assertArraysEqual = function(actual, expected) {
   eqArrays(actual, expected) ?
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+assertArraysEqual([1,2,3], [1,3,3]);
