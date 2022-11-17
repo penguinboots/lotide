692b0d473ec5ce2cc96d07c6b53e0e48619d1c2f
penguinboots <sabrina.ynw@gmail.com>
2022-11-16 20:47:44 -0800

    Added case for empty array in eqArrays

--- middle.js
+++ middle.js
@@ -1,5 +1,8 @@
 // Return true if arrays are equal
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
