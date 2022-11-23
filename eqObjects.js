9386c8f5a0012404e3553710a4c93bfeda3d01d1
penguinboots <sabrina.ynw@gmail.com>
2022-11-23 11:13:29 -0800

    initial implementation of eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,32 @@
+// Check if two values are equal
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) { // check number of keys match
+    return false;
+  }
+  for (const key of Object.keys(obj1)) { // return false if obj2 does not contain each key in obj1
+    if (!(key in obj2)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
