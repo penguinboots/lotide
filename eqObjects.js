a8d7557af5d4b8bb97520568ba290e2932bb4a3f
penguinboots <sabrina.ynw@gmail.com>
2022-11-23 11:31:25 -0800

    add array comparison in eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -5,12 +5,35 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+// Return true if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) { // check number of keys match
     return false;
   }
-  for (const key of Object.keys(obj1)) { // return false if obj2 does not contain each key in obj1
-    if (!(key in obj2)) {
+  for (const key of Object.keys(obj1)) {
+    if (!(key in obj2)) { // return false if obj2 does not contain each key in obj1
+      return false;
+    }
+    if (typeof obj1[key] !== typeof obj2[key]) { // return false if value type mismatch
+      return false;
+    }
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) { // return false if both values arrays, and arrays do not match
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) { // return false if any primitive value of key does not match opposing
       return false;
     }
   }
@@ -26,7 +49,7 @@ assertEqual(eqObjects(ab, abc), false);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
+assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, cd2), false);
