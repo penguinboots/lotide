412b0279124cd4d5f1dd65416cf363e7dd5c8540
penguinboots <sabrina.ynw@gmail.com>
2022-11-23 11:36:12 -0800

    Add inspect for print clarity

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -42,9 +42,10 @@ const eqObjects = function(obj1, obj2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   eqObjects(actual, expected) ?
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`) :
+    console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 };
 
 assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: "3" });
