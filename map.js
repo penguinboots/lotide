0f7eac86473c8b286e0a8b0eb6c5d0187ca0999b
penguinboots <sabrina.ynw@gmail.com>
2022-11-26 17:04:17 -0800

    Add assertArraysEqual, test cases

--- map.js
+++ map.js
@@ -1,3 +1,23 @@
+// Return true if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Check if two arrays are equal
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -11,3 +31,5 @@ const words = ["ground", "control", "to", "major", "tom"];
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
