cae878a49fc40ecfda4b073ca2127872bb1d9a47
penguinboots <sabrina.ynw@gmail.com>
2022-12-06 17:27:45 -0800

    refactor middle

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-// Return true if arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// Check if two arrays are equal
-const assertArraysEqual = function(actual, expected) {
-  eqArrays(actual, expected) ?
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
 
 // Return the middle value(s) of given array, return empty array if given array of length 1 or 2
 const middle = function(arr) {
@@ -31,9 +12,4 @@ const middle = function(arr) {
   return newMid;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
