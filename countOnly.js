b8ffc3554bcba789eed9326b49237cd237541150
penguinboots <sabrina.ynw@gmail.com>
2022-12-08 10:50:24 -0800

    remove tests, refactor

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,3 @@
-// // Check if two values are equal
-// const assertEqual = function(actual, expected) {
-//   actual === expected ?
-//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
-//     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-// };
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
@@ -23,22 +16,3 @@ const countOnly = function(allItems, itemsToCount) {
 
 module.exports = countOnly;
 
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
--- flatten.js
+++ flatten.js
@@ -1,23 +1,3 @@
-// // Return true if arrays are equal
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// // Check if two arrays are equal
-// const assertArraysEqual = function(actual, expected) {
-//   eqArrays(actual, expected) ?
-//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
-//     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-// };
-
 // Given array with other arrays inside, return flattened single-level array
 const flatten = function(arr) {
   let flattened = [];
@@ -32,8 +12,3 @@ const flatten = function(arr) {
 };
 
 module.exports = flatten;
-
-
-
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6], "a", ["a", 2]]), [1, 2, 3, 4, 5, 6, "a", "a", 2]);
