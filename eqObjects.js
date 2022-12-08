cd5e66c5cd24090df106bad7ab463bf3cee418fd
penguinboots <sabrina.ynw@gmail.com>
2022-12-08 12:05:45 -0800

    refactor with require/export

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,3 @@
-const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 const eqObjects = function(obj1, obj2) {
@@ -23,16 +22,4 @@ const eqObjects = function(obj1, obj2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+module.exports = eqObjects;
